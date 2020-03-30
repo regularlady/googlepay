7d90f23256e08892e41681bc4b0629150a214b36
Brittany Martin <brittany.jill.martin@gmail.com>
2020-03-29 20:51:46 -0400

    Fetch Google Pay auth token with generated JSON certificate

---
+++ lib/googlepay.rb
@@ -0,0 +1,26 @@
+require 'googleauth'
+
+module Googlepay
+
+  SCOPE = 'https://www.googleapis.com/auth/wallet_object.issuer'
+
+  def self.token
+    fetch_token
+  end
+
+  private
+ 
+  def self.fetch_token
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open("#{root}/gpay.json"),
+      scope: SCOPE
+    )
+  
+    authorizer.fetch_access_token!['access_token']
+  end
+
+  def self.root
+    File.expand_path '../..', __FILE__
+  end
+
+end
